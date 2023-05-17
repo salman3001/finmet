a9bfb82e804a418317d279466c3fa9caf6e9e2e4
Salman <therodfighter@gmail.com>
2023-05-17 09:17:53 +0400

    adeed modal close onlick on backdrop

--- src/assets/components/Modal.tsx
+++ src/assets/components/Modal.tsx
@@ -10,8 +10,18 @@ const Modal = (prop: {
   return (
     <>
       {prop.state && (
-        <div className="fixed  top-0 left-0 w-screen h-screen z-[100] bg-black  bg-opacity-70 px-6 sm:px-12 md:px-20 lg:px-32 xl:px-56 2xl:px-96 pt-20">
-          <div className="relative bg-white px-5 md:px-16 lg:px-20 xl:24 py-8 md:py-10 lg:py-16 rounded-lg pt-10">
+        <div
+          className="fixed  top-0 left-0 w-screen h-screen z-[100] bg-black  bg-opacity-70 px-6 sm:px-12 md:px-20 lg:px-32 xl:px-56 2xl:px-96 pt-20"
+          onClick={() => {
+            prop.close("youtube-Video");
+          }}
+        >
+          <div
+            className="relative bg-white px-5 md:px-16 lg:px-20 xl:24 py-8 md:py-10 lg:py-16 rounded-lg pt-10"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
             {prop.content === "youtube-Video" && <YoutubeVideo />}
             <div className="absolute top-2 right-2 cursor-pointer">
               <RiCloseCircleFill
